8fbde41f3ac6b137ae056ca9a7da4e8c2fb2ae84
vadshos <va.vy.shostak@gmail.com>
2020-11-23 16:32:21 +0200

    oop homework 3

---
+++ HomeWork OOP 3/HomeWork OOP 3/Reservoirs.cpp
@@ -0,0 +1,78 @@
+#include "Reservoirs.h"
+
+const string Reservoirs::getTypeStr() const {
+	static const string typeReservoirs[]{ "lake","sea","ocean","swamp","river","other","undefined" };
+	return typeReservoirs[(int)type];
+
+}
+void Reservoirs::Print() const
+{
+	cout << "Name " << this->Name << endl;
+	cout << "Type " << getTypeStr() << endl;
+	cout << "Width " << this->width<<endl;
+	cout << "Height " << this->height<<endl;
+	cout << "Max depth" << this->maxDepth << endl;
+	cout << "Amoult " << this->amount << endl;
+	cout << "Square "<<this->square << endl;
+
+}
+
+void Reservoirs::setName(string Name)
+{
+	if (Name != "") {
+		this->Name = Name;
+	}
+}
+
+void Reservoirs::setType(TypeReservoirs type)
+{
+	if (type >= TypeReservoirs::lake && type <= TypeReservoirs::undefined) {
+		this->type = type;
+	}
+	else {
+		this->type = TypeReservoirs::undefined;
+	}
+}
+
+void Reservoirs::setSizeReservoirs (float width, float height, float maxDepth) {
+	if (width != 0 && height != 0 && maxDepth != 0) {
+	this->width = width;
+	this->height = height;
+	this->maxDepth = maxDepth;
+	this->amount = (width * height * maxDepth);
+	this->square = (width * height);
+	}
+}
+
+bool Reservoirs::comparisonType(const Reservoirs& one, const Reservoirs& two)
+{
+	if (one.type == two.type) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+int Reservoirs::comparisonAmoult(const Reservoirs& one, const Reservoirs& two)
+{
+	if (one.amount > two.amount) {
+		return 1;
+	}
+	else if (one.amount < two.amount) {
+		return -1;
+	}
+	else {
+		return 0;
+	}
+}
+
+Reservoirs Reservoirs::TheLargestAmoult(const Reservoirs* one, TypeReservoirs type)
+{
+	Reservoirs maxReservoirs = one[0];
+	for (int i = 0; i < QuntityReservoirs; i++) {
+		if (one->amount > maxReservoirs.amount) {
+			maxReservoirs = one[i];
+		}
+	}
+}
---
+++ HomeWork OOP 3/HomeWork OOP 3/Reservoirs.h
@@ -0,0 +1,60 @@
+#pragma once
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class Reservoirs
+{
+public:
+	enum class  TypeReservoirs{lake,sea,ocean,swamp,river,other,undefined};
+	const  string getTypeStr() const;
+	void Print() const;
+	void setName(string Name);
+	void setType(TypeReservoirs type);
+	void setSizeReservoirs(float width, float height, float maxDepth); \
+	static bool comparisonType(const  Reservoirs& one, const  Reservoirs& two);
+	static int comparisonAmoult(const  Reservoirs& one, const  Reservoirs& two);
+	static Reservoirs TheLargestAmoult(const  Reservoirs* one,TypeReservoirs type);
+	string getName() const;
+	TypeReservoirs getType() const;
+	float* getSizeReservoirs() const;
+	Reservoirs(string Name, float width, float height, float maxDepth, TypeReservoirs type = TypeReservoirs::undefined);
+	Reservoirs();
+private:
+	string Name;
+	TypeReservoirs type;
+	float width = 0;
+	float height = 0;
+	float maxDepth = 0;
+	float amount = 0;
+	float square = 0;
+	static size_t QuntityReservoirs;
+};
+inline string Reservoirs::getName() const {
+	return Name;
+}
+
+inline Reservoirs::TypeReservoirs Reservoirs::getType() const
+{
+	return type;
+}
+
+inline float* Reservoirs::getSizeReservoirs() const {
+	float size[] = {width,height,maxDepth};
+	return size;
+}
+
+inline Reservoirs::Reservoirs(string Name, float width, float height, float maxDepth, TypeReservoirs type)
+{
+	    QuntityReservoirs++;
+		setName(Name);
+		setType(type);
+		setSizeReservoirs(width, height, maxDepth);
+}
+
+inline Reservoirs::Reservoirs() : Reservoirs("NoName",1,1,1,TypeReservoirs::undefined)
+{
+}
+
+
